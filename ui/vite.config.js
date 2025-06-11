54774001b9b2f0dd9922957cfd8a4d3e5f8e2e81
GioGimic <matthewatoope@gmail.com>
2025-06-11 21:13:45 -0230

    feat: update main application entry point, rename main CSS file, and add new style definitions

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/main.js')
+        main: resolve(__dirname, 'index.html')
       },
       output: {
         entryFileNames: 'assets/[name].js',
